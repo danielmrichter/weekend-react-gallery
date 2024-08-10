529c8f1339f93070910745f8a5fe9bdea1229060
Daniel Richter <drichter3697@gmail.com>
2024-08-10 15:13:47 -0500

    completed form element

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { useState, useEffect } from "react";
 import GalleryList from "../GalleryList/GalleryList";
+import PostImageForm from "../PostImageForm/PostImageForm";
 function App() {
   useEffect(() => {
     getGallery()
@@ -11,13 +12,12 @@ function App() {
     .then(res => {setGallery(res.data)})
     .catch(err => {console.log(`Error in GET/api/gallery!`, err)})
   }
-
     return (
       <div data-testid="app">
         <header>
           <h1>React Gallery</h1>
         </header>
-
+        <PostImageForm getGallery={getGallery}/>
         <p>The gallery goes here!</p>
         <GalleryList gallery={gallery} getGallery={getGallery}/>
       </div>
